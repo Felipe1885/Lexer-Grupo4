cc9f2f03643bea5ba22999d69b813950242673ca
Asdfghc <lucashmeng@gmail.com>
2026-09-02 11:15:30 -0300

    fix iterator

--- Lexer.py
+++ Lexer.py
@@ -100,32 +100,32 @@ class Lexer:
                 i += 1
             elif char in ['(', ')', '{', '}', ',', ';', '+', '-', '*', '%']: # Tokens de um caractere
                 if char == '(':
-                    tokens.append(Token(TokenKind.LEFT_PAREN, char, None, line, column))
+                    yield Token(TokenKind.LEFT_PAREN, char, None, line, column)
                 elif char == ')':
-                    tokens.append(Token(TokenKind.RIGHT_PAREN, char, None, line, column))
+                    yield Token(TokenKind.RIGHT_PAREN, char, None, line, column)
                 elif char == '{':
-                    tokens.append(Token(TokenKind.LEFT_BRACE, char, None, line, column))
+                    yield Token(TokenKind.LEFT_BRACE, char, None, line, column)
                 elif char == '}':
-                    tokens.append(Token(TokenKind.RIGHT_BRACE, char, None, line, column))
+                    yield Token(TokenKind.RIGHT_BRACE, char, None, line, column)
                 elif char == ',':
-                    tokens.append(Token(TokenKind.COMMA, char, None, line, column))
+                    yield Token(TokenKind.COMMA, char, None, line, column)
                 elif char == ';':
-                    tokens.append(Token(TokenKind.SEMICOLON, char, None, line, column))
+                    yield Token(TokenKind.SEMICOLON, char, None, line, column)
                 elif char == '+':
-                    tokens.append(Token(TokenKind.PLUS, char, None, line, column))
+                    yield Token(TokenKind.PLUS, char, None, line, column)
                 elif char == '-':
-                    tokens.append(Token(TokenKind.MINUS, char, None, line, column))
+                    yield Token(TokenKind.MINUS, char, None, line, column)
                 elif char == '*':
-                    tokens.append(Token(TokenKind.STAR, char, None, line, column))
+                    yield Token(TokenKind.STAR, char, None, line, column)
                 elif char == '%':
-                    tokens.append(Token(TokenKind.PERCENT, char, None, line, column))
+                    yield Token(TokenKind.PERCENT, char, None, line, column)
                 i += 1
                 column += 1
             elif char.isdigit(): # Literais inteiros
                 while i < len(self.source) and self.source[i].isdigit():
                     current_lexeme += self.source[i]
                     i += 1
-                tokens.append(Token(TokenKind.INT_LITERAL, current_lexeme, int(current_lexeme), line, column))
+                yield Token(TokenKind.INT_LITERAL, current_lexeme, int(current_lexeme), line, column)
                 column += len(current_lexeme)
             elif char == '"': # Literais de string
                 i += 1
@@ -143,7 +143,7 @@ class Lexer:
                     raise LexerError("string literal não termina com aspas", line, column)
                 if i < len(self.source) and self.source[i] == '"':
                     value = current_lexeme.encode().decode('unicode_escape')
-                    tokens.append(Token(TokenKind.STRING_LITERAL, f'"{current_lexeme}"', value, line, column))
+                    yield Token(TokenKind.STRING_LITERAL, f'"{current_lexeme}"', value, line, column)
                 i += 1
                 column += len(current_lexeme) + 1
             elif (char.isalpha() and char.isascii()) or char == '_': # Identificadores ou keywords
@@ -151,63 +151,63 @@ class Lexer:
                     current_lexeme += self.source[i]
                     i += 1
                 if current_lexeme in ['int', 'bool', 'void', 'if', 'else', 'while', 'return', 'print']:
-                    tokens.append(Token(TokenKind[f'KW_{current_lexeme.upper()}'], current_lexeme, None, line, column))
+                    yield Token(TokenKind[f'KW_{current_lexeme.upper()}'], current_lexeme, None, line, column)
                 elif current_lexeme in ['true']:
-                    tokens.append(Token(TokenKind[f'KW_{current_lexeme.upper()}'], current_lexeme, True, line, column))
+                    yield Token(TokenKind[f'KW_{current_lexeme.upper()}'], current_lexeme, True, line, column)
                 elif current_lexeme in ['false']:
-                    tokens.append(Token(TokenKind[f'KW_{current_lexeme.upper()}'], current_lexeme, False, line, column))
+                    yield Token(TokenKind[f'KW_{current_lexeme.upper()}'], current_lexeme, False, line, column)
                 else:
-                    tokens.append(Token(TokenKind.IDENTIFIER, current_lexeme, current_lexeme, line, column))
+                    yield Token(TokenKind.IDENTIFIER, current_lexeme, current_lexeme, line, column)
                 column += len(current_lexeme)
             elif char in ['<', '>', '=', '!', '&', '|']: # Operadores compostos
                 if i + 1 < len(self.source):
                     next_char = self.source[i + 1]
                     if char == '<' and next_char == '=':
-                        tokens.append(Token(TokenKind.LESS_EQUAL, '<=', None, line, column))
+                        yield Token(TokenKind.LESS_EQUAL, '<=', None, line, column)
                         i += 1
                         column += 1
                     elif char == '>' and next_char == '=':
-                        tokens.append(Token(TokenKind.GREATER_EQUAL, '>=', None, line, column))
+                        yield Token(TokenKind.GREATER_EQUAL, '>=', None, line, column)
                         i += 1
                         column += 1
                     elif char == '=' and next_char == '=':
-                        tokens.append(Token(TokenKind.EQUAL_EQUAL, '==', None, line, column))
+                        yield Token(TokenKind.EQUAL_EQUAL, '==', None, line, column)
                         i += 1
                         column += 1
                     elif char == '!' and next_char == '=':
-                        tokens.append(Token(TokenKind.NOT_EQUAL, '!=', None, line, column))
+                        yield Token(TokenKind.NOT_EQUAL, '!=', None, line, column)
                         i += 1
                         column += 1
                     elif char == '&' and next_char == '&':
-                        tokens.append(Token(TokenKind.LOGICAL_AND, '&&', None, line, column))
+                        yield Token(TokenKind.LOGICAL_AND, '&&', None, line, column)
                         i += 1
                         column += 1
                     elif char == '|' and next_char == '|':
-                        tokens.append(Token(TokenKind.LOGICAL_OR, '||', None, line, column))
+                        yield Token(TokenKind.LOGICAL_OR, '||', None, line, column)
                         i += 1
                         column += 1
                     else:
                         if char == '<':
-                            tokens.append(Token(TokenKind.LESS, '<', None, line, column))
+                            yield Token(TokenKind.LESS, '<', None, line, column)
                         elif char == '>':
-                            tokens.append(Token(TokenKind.GREATER, '>', None, line, column))
+                            yield Token(TokenKind.GREATER, '>', None, line, column)
                         elif char == '=':
-                            tokens.append(Token(TokenKind.ASSIGN, '=', None, line, column))
+                            yield Token(TokenKind.ASSIGN, '=', None, line, column)
                         elif char == '!':
-                            tokens.append(Token(TokenKind.LOGICAL_NOT, '!', None, line, column))
+                            yield Token(TokenKind.LOGICAL_NOT, '!', None, line, column)
                         elif char == '&':
                             raise LexerError("operador '&' não é válido sozinho", line, column)
                         elif char == '|':
                             raise LexerError("operador '|' não é válido sozinho", line, column)
                 else:
                     if char == '<':
-                        tokens.append(Token(TokenKind.LESS, '<', None, line, column))
+                        yield Token(TokenKind.LESS, '<', None, line, column)
                     elif char == '>':
-                        tokens.append(Token(TokenKind.GREATER, '>', None, line, column))
+                        yield Token(TokenKind.GREATER, '>', None, line, column)
                     elif char == '=':
-                        tokens.append(Token(TokenKind.ASSIGN, '=', None, line, column))
+                        yield Token(TokenKind.ASSIGN, '=', None, line, column)
                     elif char == '!':
-                        tokens.append(Token(TokenKind.LOGICAL_NOT, '!', None, line, column))
+                        yield Token(TokenKind.LOGICAL_NOT, '!', None, line, column)
                     elif char == '&':
                         raise LexerError("operador '&' não é válido sozinho", line, column)
                     elif char == '|':
@@ -238,18 +238,18 @@ class Lexer:
                         else:
                             raise LexerError("comentário de bloco não termina", ini_line, ini_column)
                     else:
-                        tokens.append(Token(TokenKind.SLASH, '/', None, line, column))
+                        yield Token(TokenKind.SLASH, '/', None, line, column)
                         i += 1
                         column += 1
                 else:
-                    tokens.append(Token(TokenKind.SLASH, '/', None, line, column))
+                    yield Token(TokenKind.SLASH, '/', None, line, column)
                     column += 1
             else:
                 raise LexerError(f"caractere inesperado {char}", line, column)
 
             current_lexeme = ""
 
-        tokens.append(Token(TokenKind.EOF, "", None, line, column))
+        yield Token(TokenKind.EOF, "", None, line, column)
         
         return tokens
 
